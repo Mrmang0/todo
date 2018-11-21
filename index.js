511443fb17fb95ef02e0b5708eabb7631547f026
Oleksandr Babii <oba@CDMUkraine.Local>
2018-11-21 16:39:50 +0200

    sounds like the end

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-function toDoNote(name, description) {
+function toDoNote(name, description,removeFunction,date) {
     var self = this;
     self.name = ko.observable(name);
     self.description = ko.observable(description);
-    let tDate = new Date();
-    self.date = ko.observable(tDate.getHours() + ':' + tDate.getMinutes() + " " + tDate.getDate() + '.' + tDate.getMonth() + '.' + tDate.getFullYear());
+    let oldValues = updateValues();
+    self.date = ko.observable(date === undefined?updateDate(new Date()) :date);
     self.isVisible = ko.observable(false);
+
     self.changeMode = function () {
         self.isVisible(!self.isVisible());
+        if (!self.isVisible()) {
+            self.name(oldValues.name);
+            self.description(oldValues.description);
+        }
     }
-    self.deleteNote =function(root){
-    console.log(    root.indexOf(self));
+
+    self.editNote = function () {
+        oldValues = updateValues();
+        self.isVisible(!self.isVisible());
+        self.date("edited: " + updateDate(new Date()))
     }
+
+    self.deleteNote = function () {
+        removeFunction(this);
+    }
+
+    function updateValues() {
+        return {
+            name: self.name(),
+            description: self.description()
+        }
+    }
+
+    function updateDate(sDate) {
+        return sDate.getHours() + ':' +
+            sDate.getMinutes() + " " + sDate.getDate() + '.' + sDate.getMonth() + '.' + sDate.getFullYear();
+    }
+
 }
 
 
@@ -18,33 +43,75 @@ function toDoViewModel() {
     var self = this;
     self.name = ko.observable("");
     self.description = ko.observable("");
+
     self.displayNoteForm = ko.observable(false);
+    self.displayError = ko.observable(false);
 
-
-    // Editable data
-    self.toDos = ko.observableArray([
-        new toDoNote("first", "just nothing"),
-        new toDoNote("second", "still nothing",self)
-    ]);
-
-    self.addNewNote = function () {
-
-        if (!self.displayNoteForm()) {
-            console.log(self.displayNoteForm())
-            self.displayNoteForm(!self.displayNoteForm());
-        } else {
-            self.toDos.push(new toDoNote(self.name(), self.description()));
-            self.displayNoteForm(!self.displayNoteForm())
-            self.name("");
-            self.description("");
-
-        }
-
+    self.deleteNote = function (note) {
+        self.notes.splice(self.notes.indexOf(note), 1);
+        saveNotesList();
     }
 
 
+    self.notes =ko.observableArray([
+        new toDoNote("first", "just nothing",self.deleteNote),
+        new toDoNote("second", "heh",self.deleteNote)
+    ]);
 
 
+    
+
+    self.addNewNote = function () {
+        if (!self.displayNoteForm()) {
+            self.displayNoteForm(!self.displayNoteForm());
+        } else {
+            if (self.name() === "" || self.description() === "") {
+                self.displayError(true);
+                return;
+            }
+            
+            self.notes.push(new toDoNote(self.name(), self.description()
+            ,self.deleteNote));
+            self.displayNoteForm(!self.displayNoteForm())
+            self.name("");
+            self.description("");
+            self.displayError(false);
+            saveNotesList();
+        }
+    }
+
+    self.changeMode = function () {
+        self.displayNoteForm(!self.displayNoteForm());
+    }
+
+    function isExist(){
+        localStorage.getItem('notes');
+    }
+
+    function saveNotesList()
+    {
+        const notesList =[];
+        self.notes().forEach(element => {
+            notesList.push({
+                name: element.name(),
+                description: element.description(),
+                date: element.date()
+            })
+        });
+        console.log("closed");
+        localStorage.setItem('notes',JSON.stringify(notesList));    
+    }
+
+    function getNotesList()
+    {
+        const notesList =JSON.parse(localStorage.getItem('notes'));
+        const noteFuncList  = [];
+        notesList.forEach(element => {
+            noteFuncList.push(new toDoNote(element.name,element.description,
+                self.deleteNote,element.date))
+        });
+        return noteFuncList;
+    }
 }
 
 ko.bindingHandlers.fadeVisible = {
